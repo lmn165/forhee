ad486cc728505b3a46c4596dbeb5493390ded486
btw5189 <111111>
2017-09-20 17:52:08 +0900

    calendar 백태운

---
+++ WebContent/sql/calendar.sql
@@ -0,0 +1,11 @@
+-- 생성
+create table T2_MEMBER(
+	c_no		  number(10),
+	title         varchar2(100),
+	content       varchar2(4000),
+   	start         date,
+	end           date,
+	day           date
+);
+
+
